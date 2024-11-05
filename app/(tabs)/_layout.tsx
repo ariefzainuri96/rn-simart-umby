8d5c4e8ce5eaebcec902ef14b8479ef7b8976237
Arief Zainuri <flaminglassoo1996@gmail.com>
2024-11-05 10:30:32 +0700

    update: 1. setup tailwind nativewind for react native

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -13,9 +13,10 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-      }}>
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name='index'
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
@@ -24,7 +25,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name='explore'
         options={{
           title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
